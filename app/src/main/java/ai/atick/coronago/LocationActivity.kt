1e00d6de89985e54274d79039d0df39d82bfd11d
Atick Faisal <atickfaisal@gmail.com>
2020-04-19 10:32:01 +0600

    location class

--- app/src/main/java/ai/atick/coronago/LocationActivity.kt
+++ app/src/main/java/ai/atick/coronago/LocationActivity.kt
@@ -1,20 +1,27 @@
 package ai.atick.coronago
 
 import android.content.Context
+import android.widget.Toast
 import com.google.android.gms.location.LocationServices
 import java.text.SimpleDateFormat
 import java.util.*
 
-class LocationActivity(context: Context) {
+class LocationActivity(private val context: Context) {
 
-    var latitude: String = "24.00001"
+    var latitude: String = "20.00001"
     var longitude: String = "90.00001"
 
     init {
+        this.latitude = "20.50500"
         LocationServices
             .getFusedLocationProviderClient(context)
             .lastLocation
             .addOnSuccessListener { location ->
+                Toast.makeText(
+                    context,
+                    "Latitude: ${location.latitude}",
+                    Toast.LENGTH_LONG
+                ).show()
                 this.latitude = location.latitude.toString()
                 this.longitude = location.longitude.toString()
             }
--- app/src/main/java/ai/atick/coronago/MainActivity.kt
+++ app/src/main/java/ai/atick/coronago/MainActivity.kt
@@ -1,13 +1,21 @@
 package ai.atick.coronago
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var networkActivity: NetworkActivity
     private lateinit var locationActivity: LocationActivity
+
+    private val uploadTaskId = "Location Upload"
 
     private var userUrl: String = "http://home.jamiussiam.com:8090/user"
     private var locationUrl: String = "http://home.jamiussiam.com:8090/location"
@@ -17,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     private var phone = "01711010101"
     private var gender = "OTHER"
     private var birthDate = "01-01-1969"
+    private val channelId = "101010"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +56,35 @@ class MainActivity : AppCompatActivity() {
                 url = locationUrl,
                 data = locationData
             )
+        }
+
+        createPeriodicTasks()
+        createNotificationChannel()
+    }
+
+    private fun createPeriodicTasks() {
+        val uploadTask = PeriodicWorkRequestBuilder<UploadActivity>(
+            15, TimeUnit.MINUTES
+        ).build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            uploadTaskId,
+            ExistingPeriodicWorkPolicy.KEEP,
+            uploadTask
+        )
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
